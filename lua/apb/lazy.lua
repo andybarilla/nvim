3a5ffac2dd8acdd7a487cc4acb116e65d8ef4ec5
Andy Barilla <andy@berp.me>
2024-01-26 18:28:05 -0700

    fix mason lspconfig

--- lua/apb/lazy.lua
+++ lua/apb/lazy.lua
@@ -1,108 +1,108 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"bluz71/vim-moonfly-colors",
-		config = function()
-			vim.o.background = "dark"
-			vim.cmd("colorscheme moonfly")
-		end,
-	},
-	"nvim-lua/plenary.nvim",
-	"nvim-telescope/telescope.nvim",
-	{
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	},
-	"nvim-treesitter/playground",
-	"theprimeagen/harpoon",
-	"github/copilot.vim",
-	"nvim-tree/nvim-web-devicons",
-	"sbdchd/neoformat",
-	{
-		"nvim-lualine/lualine.nvim",
-		opt = true,
-	},
-	"joerdav/templ.vim",
-	"nvimtools/none-ls.nvim",
-	{
-		"folke/trouble.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"roobert/tailwindcss-colorizer-cmp.nvim",
-		},
-	},
-	"ray-x/go.nvim",
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williambowan/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/nvim-cmp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-		},
-	},
-	{
-		"roobert/tailwindcss-colorizer-cmp.nvim",
-		config = function()
-			require("tailwindcss-colorizer-cmp").setup({
-				color_square_width = 2,
-			})
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
-	"ThePrimeagen/vim-be-good",
+    {
+        "bluz71/vim-moonfly-colors",
+        config = function()
+            vim.o.background = "dark"
+            vim.cmd("colorscheme moonfly")
+        end,
+    },
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    },
+    "nvim-treesitter/playground",
+    "theprimeagen/harpoon",
+    "github/copilot.vim",
+    "nvim-tree/nvim-web-devicons",
+    "sbdchd/neoformat",
+    {
+        "nvim-lualine/lualine.nvim",
+        opt = true,
+    },
+    "joerdav/templ.vim",
+    "nvimtools/none-ls.nvim",
+    {
+        "folke/trouble.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {},
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "roobert/tailwindcss-colorizer-cmp.nvim",
+        },
+    },
+    "williamboman/mason-lspconfig",
+    "ray-x/go.nvim",
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+    },
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        config = function()
+            require("tailwindcss-colorizer-cmp").setup({
+                color_square_width = 2,
+            })
+        end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
+    "ThePrimeagen/vim-be-good",
 })
 
 vim.cmd("autocmd BufWritePre * Neoformat")
